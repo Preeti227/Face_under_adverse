65b45009fa0363efca01d5dbbe970037be055db1
Preeti <preetidey227@gmail.com>
2025-10-18 09:44:01 +0530

    denoise filter

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,4 +1,9 @@
 import cv2
+import os
+import matplotlib.pyplot as plt
+import tempfile
+TEMP_SAVE_PATH = os.path.join(tempfile.gettempdir(), "denoised_temp.jpg")
+
 def enhanced_distortion_filters(img, distortion_type=None):
     img = cv2.resize(img, (160, 160))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -19,3 +24,28 @@ def enhanced_distortion_filters(img, distortion_type=None):
         img = cv2.GaussianBlur(img, (3, 3), 0)
         img = cv2.detailEnhance(img, sigma_s=5, sigma_r=0.15)
     return img
+
+def denoise_if_needed(image_path, distortion_type):
+    img = cv2.imread(image_path)
+    if img is None:
+        raise ValueError(f"Image not found: {image_path}")
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    if distortion_type in ['noise', 'rain']:
+        denoised = cv2.medianBlur(img_rgb, 5)
+        denoised_bgr = cv2.cvtColor(denoised, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(TEMP_SAVE_PATH, denoised_bgr)
+        plt.figure(figsize=(10, 4))
+        plt.subplot(1, 2, 1)
+        plt.imshow(img_rgb)
+        plt.title('Original (Distorted)')
+        plt.axis('off')
+        plt.subplot(1, 2, 2)
+        plt.imshow(denoised)
+        plt.title('Denoised Output')
+        plt.axis('off')
+        plt.tight_layout()
+        plt.show()
+        return TEMP_SAVE_PATH
+    else:
+        return image_path
